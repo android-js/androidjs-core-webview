99948373ea077b6f65b8dc4cf9f3ef830b7adeb4
Chhekur <820121223505e@gmail.com>
2019-04-20 18:14:31 +0530

    add: Toast API

---
+++ app/src/main/java/com/android/js/api/Toast.java
@@ -0,0 +1,12 @@
+package com.android.js.api;
+
+import android.app.Activity;
+
+public class Toast {
+    public static void showToast(Activity activity, String text, int duration){
+        if(duration > 1) duration = 1;
+        if(duration < 0) duration = 0;
+        android.widget.Toast tost = android.widget.Toast.makeText(activity, text, duration);
+        tost.show();
+    }
+}
